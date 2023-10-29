4b6fcbc7771e70643faeae7bcce17126fa2e3dbc
HussainMBH <mbahirh624@gmail.com>
2023-10-29 18:49:10 +0530

    move_zeros_end_BFA

---
+++ src/Move_zeros_end/move_zeros_end_BFA.java
@@ -0,0 +1,41 @@
+package Move_zeros_end;
+import java.util.*;
+
+public class move_zeros_end_BFA {
+    public static int[] moveZeros(int n, int []a) {
+        //temporary array:
+        ArrayList<Integer> temp = new ArrayList<>();
+        //copy non-zero elements
+        //from original -> temp array:
+        for (int i = 0; i < n; i++) {
+            if (a[i] != 0)
+                temp.add(a[i]);
+        }
+
+        // number of non-zero elements.
+        int nz = temp.size();
+
+        //copy elements from temp
+        //fill first nz fields of
+        //original array:
+        for (int i = 0; i < nz; i++) {
+            a[i] = temp.get(i);
+        }
+
+        //fill rest of the cells with 0:
+        for (int i = nz; i < n; i++) {
+            a[i] = 0;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        int n = 10;
+        int[] ans = moveZeros(n, arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println("");
+    }
+}
