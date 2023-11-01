df0913452fca2da1123373bded820459cdca0d2e
HussainMBH <mbahirh624@gmail.com>
2023-11-01 18:37:47 +0530

    Array Listing

---
+++ src/ArraySection/ArrayListLearn.java
@@ -0,0 +1,14 @@
+package ArraySection;
+
+import java.util.ArrayList;
+
+public class ArrayListLearn {
+    public static void main(String[] args){
+        ArrayList<String> ar = new ArrayList<String>();
+        ar.add("Mohamed");
+        ar.add("Bahir");
+        ar.add("Hussain");
+        System.out.println(ar);
+        System.out.println(ar.size());
+    }
+}
