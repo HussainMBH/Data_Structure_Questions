018fbbaf2f5cfdd8438d2f25bcac377588abaebf
HussainMBH <mbahirh624@gmail.com>
2023-11-02 13:37:11 +0530

    HashSet

---
+++ src/Set_Interface/hashset.java
@@ -0,0 +1,16 @@
+package Set_Interface;
+
+import java.util.HashSet;
+
+public class hashset {
+    public static void main(String[] args){
+        HashSet hs = new HashSet();
+        hs.add(5);
+        hs.add(10);
+        hs.add(3);
+        hs.add(4);
+        hs.add(7);
+        hs.add(8);
+        System.out.println(hs);
+    }
+}
