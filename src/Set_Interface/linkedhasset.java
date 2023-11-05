a9e9071916aa941b38d598813f30339a1f6c5d2c
HussainMBH <mbahirh624@gmail.com>
2023-11-05 20:42:04 +0530

    LinkedList add in LinkedHashSet

--- src/Set_Interface/linkedhasset.java
+++ src/Set_Interface/linkedhasset.java
@@ -4,6 +4,7 @@ LinkedHashSet is a insertion order maintain
 package Set_Interface;
 
 import java.util.LinkedHashSet;
+import java.util.LinkedList;
 
 public class linkedhasset {
     public static void main(String[] args) {
@@ -16,5 +17,21 @@ public class linkedhasset {
         hs.add(8);
         System.out.println(hs);
         System.out.println("Can add number 10 duplicate element? " + hs.add(10));//checking for can add duplicate element
+
+        LinkedList ll = new LinkedList();
+        ll.add(10);
+        ll.add(20);
+        ll.add(30);
+        ll.add(30);
+        ll.add(20);
+        ll.add(40);
+        ll.add(50);
+        ll.add(50);
+
+        System.out.println();
+        System.out.println("Befor LinkedHashSet " + ll);
+        LinkedHashSet lhs = new LinkedHashSet(ll); // adding linkedlist in linkedhashset for removing duplicate
+        System.out.println("After LinkedHashSet " + lhs);
+
     }
 }
